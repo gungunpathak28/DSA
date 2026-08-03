fffc5eea196ecb1edc598cbf6d24c7a7eb04e98e
Gungun pathak <gungunpathak.2006@gmail.com>
2026-08-03 15:03:39 +0530

    Refactor SumOfDiagonalMatrix.java formatting

--- SumOfDiagonalMatrix.java
+++ SumOfDiagonalMatrix.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class SumOfDiagonalMatrix {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("\n Enter rows : ");
