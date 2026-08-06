26f16c44a72d34601e5861f75b1aad3df5a3f1e3
Gungun pathak <gungunpathak.2006@gmail.com>
2026-08-06 08:11:50 +0530

    Remove unnecessary whitespace and reformat code

--- Priorityqueue.java
+++ Priorityqueue.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Priorityqueue {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of elements: ");
